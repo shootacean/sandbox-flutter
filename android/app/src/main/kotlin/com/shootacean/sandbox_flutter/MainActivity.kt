bbb06e60ef4feec25e5e9da127e797062b387fc6
Shootacean <shootacean@gmail.com>
2020-06-20 16:35:09 +0900

    create flutter project.

---
+++ android/app/src/main/kotlin/com/shootacean/sandbox_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.shootacean.sandbox_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ android/app/src/main/kotlin/com/shootacean/sandboxflutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.shootacean.sandboxflutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
